376f95bd37983e8715d2d6ca1f33a6479f010f3d
Aditya <adijr9487@gmail.com>
2022-08-10 08:30:30 +0530

    traverse

--- binary_tree.cpp
+++ binary_tree.cpp
@@ -1,48 +1,72 @@
+//g++ -std=c++11 binary_tree.cpp -o binary_tree && ./binary_tree
 #include<iostream>
 #include<bits/stdc++.h>
 
 using namespace std;
 
-class TreeNode {
-    public:
-        int val;
-        TreeNode* left;
-        TreeNode* right;
+class TreeNode
+{
+public:
+    int val;
+    TreeNode *left;
+    TreeNode *right;
 
-        TreeNode(int val){
-            this->val = val;
-            this->left = nullptr;
-            this->right = nullptr;
-        }
-
+    TreeNode(int val)
+    {
+        this->val = val;
+        this->left = nullptr;
+        this->right = nullptr;
+    }
 };
-//             L          R 
+
+//             L          R
 //      -----------------------------
 // pre    ROOT |          |
 // in          |   ROOT   |
-// Post        |          |    ROOT    
+// Post        |          |    ROOT
 
- 
-void print_tree_io(TreeNode* root){
+void inOrderTree(TreeNode *root)
+{
+    if (root == nullptr)
+        return;
+
+    inOrderTree(root->left);
+    cout << root->val << " ";
+    inOrderTree(root->right);
+}
+
+void preOrderTree(TreeNode *root){
     if(root == nullptr)
         return;
 
-    print_tree_io(root->left);
     cout << root->val << " ";
-    print_tree_io(root->right);
+    preOrderTree(root->left);
+    preOrderTree(root->right);
+}
 
+void postOrderTree(TreeNode* root){
+    if(root == nullptr)
+        return;
+
+    postOrderTree(root->left);
+    postOrderTree(root->right);
+    cout << root->val << " ";
 }
 
 int main()
 {
-    //create tree
+    // create tree
     TreeNode *head = new TreeNode(21);
     head->left = new TreeNode(30);
     head->left->left = new TreeNode(32);
     head->left->right = new TreeNode(28);
     head->right = new TreeNode(56);
 
-    print_tree_io(head);
+    inOrderTree(head);
+    cout << "\n";
+    preOrderTree(head);
+    cout << "\n";
+    postOrderTree(head);
 
     return 0;
 }
