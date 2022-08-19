4dd1d3fbeb5265ac8b0218b28a20006dbde24ecc
Aditya <adijr9487@gmail.com>
2022-08-19 19:59:07 +0530

    partial

--- bfs.cpp
+++ bfs.cpp
@@ -4,10 +4,18 @@
 
 using namespace std;
 
-
+void print(vector<int>& v){
+    for(auto it: v)
+        cout << it << " ";
+    cout << "\n";
+}
 
 int main()
 {
-    
+    vector<int> va = {1, 2, 4, 5, 7, 8, 5, 4};
+    print(va);
+    va.clear();
+    va.resize(3, 1);
+    print(va);
     return 0;
 }
